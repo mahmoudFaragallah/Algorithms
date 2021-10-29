073158e7996ff24a896d1e43c0eb471c3496ae76
Mahmoud Faragallah <mahmoudahmedfarajallah@outlook.com>
2021-10-29 23:34:02 +0200

    Add project files.

---
+++ datastructures/dynamicarray/DynamicArray.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.datastructures.dynamicarray
+{
+    public class DynamicArray<T> 
+    {
+        private T[] array;
+        private int len = 0; //length user thinks array is
+        private int capacity = 0; // Actual array size
+
+        
+        public DynamicArray()
+        {
+           // this(16);
+        }
+
+    }
+}
